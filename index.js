a37d5096280f17f547351b5c2288d4c8d9aa4463
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-05-31 17:39:48 +0100

    The App works finnaly

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const importData = require("./quotes.json");
 let port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send(importData);
 });
 
 app.listen(port, ()=>{
-    console.log(`App is running on: https://localhost:${port}`);
+    console.log(`App is running on: localhost:${port}`);
 });
